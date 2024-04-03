0f9849d6be725c3217a847c3ee0f41ffeab16385
Shahar Amram <126160304+ShaharAmram1041@users.noreply.github.com>
2024-04-03 20:11:15 +0300

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-docker stop super-app-node
-docker rm super-app-node
+docker stop lidor-nginx
+# super-app-node
+docker rm lidor-nginx
+# super-app-node
 
 
 # docker run --name super-app-node -d -p 3000:3000 shahar1041/super-app:node
