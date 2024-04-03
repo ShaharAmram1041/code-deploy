d66fe533eb1567a99ebc7a4bb80dc83a7a5b01ee
Shahar Amram <126160304+ShaharAmram1041@users.noreply.github.com>
2024-04-03 20:51:41 +0300

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,8 +2,8 @@
 docker stop super-app-node
 docker rm super-app-node
 
-# docker stop super-app-php
-# docker rm super-app-php
+docker stop super-app-php
+docker rm super-app-php
 
 # docker stop super-app-mySQL
 # docker rm super-app-mySQL
