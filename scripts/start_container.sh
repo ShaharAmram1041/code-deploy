0fa64918ce3358f4fb5dfe50aaaefe96861183c9
Shahar Amram <126160304+ShaharAmram1041@users.noreply.github.com>
2024-04-03 19:57:29 +0300

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run --name lidor-nginx -d -p 80:80 nginx
+# docker run --name lidor-nginx -d -p 80:80 nginx
+docker run --name super-app-node -d -p 3000:3000 shahar1041/super-app:node
